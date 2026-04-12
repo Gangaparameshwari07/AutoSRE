41083eb58a157d13286dc8649bebfaf111a0c3ab
Ganga <gangapd0605@gmail.com>
2026-04-12 11:38:36 +0530

    fixing tasks foder

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,1 +1,7 @@
-from .app import app
+from .easy_task import grade_easy
+from .medium_task import grade_medium
+from .hard_task import grade_hard
+from .recovery_task import grade_recovery
+from .edge_task import grade_edge
+
+__all__ = ['grade_easy', 'grade_medium', 'grade_hard', 'grade_recovery', 'grade_edge']
